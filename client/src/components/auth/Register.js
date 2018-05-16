e27d77a079630dc1e7818e8ddd829262f35df8fb
tylergreulich <tgreul6@gmail.com>
2018-05-16 16:19:29 -0700

    Started working on the client

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   state = {
@@ -15,13 +17,23 @@ class Register extends Component {
 
   onSubmitHandler = event => {
     event.preventDefault();
+    let { name, email, password, password2 } = this.state;
+    const newUser = {
+      name,
+      email,
+      password,
+      password2
+    };
 
-    const { newUser = { name, email, password, password2 } } = this.state;
-
-    console.log(name, email, password, password2);
+    axios
+      .post('/api/auth/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   };
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="register">
         <div className="container">
@@ -35,22 +47,32 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
                     onChange={this.onChangeHandler}
                   />
+                  {errors.name ? (
+                    <div className="invalid-feedback">{errors.name}</div>
+                  ) : null}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChangeHandler}
                   />
+                  {errors.email ? (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  ) : null}
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
@@ -59,22 +81,32 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChangeHandler}
                   />
+                  {errors.password ? (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  ) : null}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChangeHandler}
                   />
+                  {errors.password2 ? (
+                    <div className="invalid-feedback">{errors.password2}</div>
+                  ) : null}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
