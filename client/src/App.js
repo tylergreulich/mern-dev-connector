27728b6f180d434473d888035a66044581b86183
tylergreulich <tgreul6@gmail.com>
2018-06-06 16:42:45 -0700

    github profile component

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import store from './store/store';
 import jwt_decode from 'jwt-decode';
@@ -80,6 +81,7 @@ class App extends Component {
                   component={AddEducation}
                 />
               </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -39,4 +39,8 @@ class ProfileAbout extends Component {
   }
 }
 
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
 export default ProfileAbout;
